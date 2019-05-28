a4948638ee2fc806f824b5cc1c1cd43b39f96999
unknown <lstshashank@gmail.com>
2019-05-28 15:59:06 +0530

    Test trigger

--- test-jenkins-build/src/main/java/com/test/build/testjenkinsbuild/TestJenkinsBuildApplication.java
+++ test-jenkins-build/src/main/java/com/test/build/testjenkinsbuild/TestJenkinsBuildApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestJenkinsBuildApplication {
 
 	public static void main(String[] args) {
+		//Main method
 		SpringApplication.run(TestJenkinsBuildApplication.class, args);
 	}
 
