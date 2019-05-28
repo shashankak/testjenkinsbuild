90dd1eb06935c85829a6b1ba7151c466fd5c7353
unknown <lstshashank@gmail.com>
2019-05-28 15:34:22 +0530

    Test commit

---
+++ test-jenkins-build/src/main/java/com/test/build/testjenkinsbuild/TestJenkinsBuildApplication.java
@@ -0,0 +1,13 @@
+package com.test.build.testjenkinsbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkinsBuildApplication.class, args);
+	}
+
+}
